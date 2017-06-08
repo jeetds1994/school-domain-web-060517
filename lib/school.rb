6ef2345db49d8e12204c0740cf67ef557cabf6f8
Diljeet <jeetds1994@gmail.com>
2017-06-08 17:23:50 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+
+class School
+
+  attr_accessor :roster, :school_name
+
+  def initialize(test)
+    @school_name = test
+    @roster = Hash.new
+  end
+
+  def add_student(name, grade)
+    if @roster[grade].class != Array
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+
+    @roster.map{|x, y| @roster[x] = @roster[x].sort}
+    @roster
+  end
+end
